156352d90a1c2a7f837f197dee74e502f423eee9
Maxim Trofimov <59147298+YABYE@users.noreply.github.com>
2024-02-05 08:12:05 +0300

    Update PlayerRepositoryDB.java

--- src/main/java/com/game/repository/PlayerRepositoryDB.java
+++ src/main/java/com/game/repository/PlayerRepositoryDB.java
@@ -32,7 +32,7 @@ public class PlayerRepositoryDB implements IPlayerRepository {
         Properties properties = new Properties();
         properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQL8Dialect");
         properties.put(Environment.USER, "root");
-        properties.put(Environment.PASS, "M20122003m");
+        properties.put(Environment.PASS, "root");
         properties.put(Environment.SHOW_SQL, "true");
         properties.put(Environment.HBM2DDL_AUTO, "update");
 
